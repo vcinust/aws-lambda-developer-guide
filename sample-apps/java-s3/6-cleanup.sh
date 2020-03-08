1ad12a03b7b74f264067e51b1019b552db600f98
Michael Wunderlich <mwunderl@amazon.com>
2020-03-08 00:47:22 -0800

    cleanup script

--- sample-apps/java-s3/6-cleanup.sh
+++ sample-apps/java-s3/6-cleanup.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 set -eo pipefail
+APP_BUCKET=$(aws cloudformation describe-stack-resource --stack-name java-s3 --logical-resource-id bucket --query 'StackResourceDetail.PhysicalResourceId' --output text)
 aws cloudformation delete-stack --stack-name java-s3
 echo "Deleted function stack"
 if [ -f bucket-name.txt ]; then
@@ -13,5 +14,13 @@ if [ -f bucket-name.txt ]; then
         esac
     done
 fi
+while true; do
+    read -p "Delete application bucket ($APP_BUCKET)?" response
+    case $response in
+        [Yy]* ) aws s3 rb --force s3://$APP_BUCKET; break;;
+        [Nn]* ) break;;
+        * ) echo "Response must start with y or n.";;
+    esac
+done
 rm -f 2-deploy.sh out.yml out.json event.json
 rm -rf build .gradle target
