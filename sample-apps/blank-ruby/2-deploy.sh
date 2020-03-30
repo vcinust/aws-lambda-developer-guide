29d95d2ac9c9c64d196d9293c0d4bebb0b8cc0a9
Michael Wunderlich <mwunderl@amazon.com>
2020-03-29 17:36:54 -0700

    working deployment

--- sample-apps/blank-ruby/2-deploy.sh
+++ sample-apps/blank-ruby/2-deploy.sh
@@ -2,7 +2,10 @@
 set -eo pipefail
 ARTIFACT_BUCKET=$(cat bucket-name.txt)
 cd function
-bundle install --path vendor/bundle
+rm -rf vendor
+rm function/Gemfile.lock
+bundle config set path 'vendor/bundle'
+bundle install
 cd ../
 aws cloudformation package --template-file template.yml --s3-bucket $ARTIFACT_BUCKET --output-template-file out.yml
 aws cloudformation deploy --template-file out.yml --stack-name blank-ruby --capabilities CAPABILITY_NAMED_IAM
